581639055c4089df48085ec6c819d99ce72171bb
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2026-02-06 17:21:55 +0000

    add about to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../../components/Hero/Hero";
+import About from "../About/About";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.home_container}>
       <Hero />
+      <About />
     </div>
   );
 };
