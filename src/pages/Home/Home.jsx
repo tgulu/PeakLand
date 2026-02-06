e6300e18eb1c3cd378d5a74d0ba6b0bf47ff291f
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2026-02-06 00:07:28 +0000

    remove navbar from home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
-import Nav from "../../components/Nav/Nav";
 import Hero from "../../components/Hero/Hero";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.home_container}>
-      <Nav />
       <Hero />
     </div>
   );
