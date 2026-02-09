a71b2bee3fb508e52808d06f16818acab90218d2
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2026-02-09 20:05:01 +0000

    remove box-shadow and unused css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,26 +2,30 @@ import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <div className={styles.hero__container}>
-      <div className={styles.hero__background}>
-        <div className={styles.hero__content}>
-          <h1 className={styles.hero__title}>
-            Peakland Professional Cleaning{" "}
-            <span className={styles.hero__span}>Services </span>{" "}
-          </h1>
+    <section id="Home">
+      <div className={styles.hero__container}>
+        <div className={styles.hero__background}>
+          <div className={styles.hero__content}>
+            <h1 className={styles.hero__title}>
+              Peakland Professional Cleaning{" "}
+              <span className={styles.hero__span}>Services </span>{" "}
+            </h1>
 
-          <p className={styles.hero__subtitle}>
-            Enjoy a spotless space with our trusted cleaning professionals. Eco
-            friendly flexible and always on time.
-          </p>
-          <div className={styles.hero__actions}>
-            <button className={styles.hero__primary}>Book Now</button>
+            <p className={styles.hero__subtitle}>
+              Enjoy a spotless space with our trusted cleaning professionals.
+              Eco friendly flexible and always on time.
+            </p>
+            <div className={styles.hero__actions}>
+              <button className={styles.hero__primary}>Book Now</button>
 
-            <button className={styles.hero__secondary}>See Our Services</button>
+              <button className={styles.hero__secondary}>
+                See Our Services
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
