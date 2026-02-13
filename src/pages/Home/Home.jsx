a46a0fbf0899893dafc5ed3569dfe828590a58ec
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2026-02-13 14:38:36 +0000

    add in new testimonals page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import Hero from "../../components/Hero/Hero";
 import About from "../About/About";
 import styles from "./Home.module.css";
+import Service from "../Services/Services";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
   return (
     <div className={styles.home_container}>
       <Hero />
       <About />
+      <Service />
+      <Testimonials />
     </div>
   );
 };
